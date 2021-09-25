8ee5dce8d5f179290aa46d351ea22cd10ebe8cd4
Denílson Pereira <deene67@gmail.com>
2021-09-25 13:50:25 -0300

    login funcional e logout nao funcional

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password))
+            return $this->returnVO("Credenciais inválidas", null);
+
+
+        $userCredentials = ([
+            'user' => $user,
+            'token' => $user->createToken($request->device_name)->plainTextToken
+        ]);
+
+
+        return $this->returnVO("Login realizado", $userCredentials);
+    }
+
+    public function logout(Request $request)
+    {
+        // Auth::logout();
+        // auth()->user()->tokens()->delete();
+
+        // $request->session()->invalidate();
+
+        // $request->session()->regenerateToken();
+
+        return $this->returnVO("Logout realizado", null);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'email' => 'required|email|unique:users',
+                'name' => 'required|max:255',
+                'password' => 'required|min:8',
+                'device_name' => 'required'
+            ]
+        );
+
+        $user = User::create($request->all());
+
+        $userCredentials = ([
+            'user' => $user,
+            'token' => $user->createToken($request->device_name)->plainTextToken
+        ]);
+
+        return $this->returnVO("Usuário Criado", $userCredentials);
+    }
+
+    private function returnVO($message, $content)
+    {
+        return response()->json(["message" => $message, "resp" => $content]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,30 +18,39 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+    Route::post('/brand', [BrandController::class, 'store']);
+    Route::put('/brand/{brand}', [BrandController::class, 'update']);
+    Route::delete('/brand/{brand}', [BrandController::class, 'destroy']);
+    Route::post('/brand/restore/{brand}', [BrandController::class, 'restore']);
+    Route::post('/brand/trash', [BrandController::class, 'getTrashed']);
+
+    Route::post('/category', [CategoryController::class, 'store']);
+    Route::put('/category/{category}', [CategoryController::class, 'update']);
+    Route::delete('/category/{category}', [CategoryController::class, 'destroy']);
+    Route::post('/category/restore/{category}', [CategoryController::class, 'restore']);
+    Route::post('/category/trash', [CategoryController::class, 'getTrashed']);
+
+    Route::post('/product', [ProductController::class, 'store']);
+    Route::put('/product/{product}', [ProductController::class, 'update']);
+    Route::delete('/product/{product}', [ProductController::class, 'destroy']);
+    Route::post('/product/restore/{product}', [ProductController::class, 'restore']);
+    Route::post('/product/trash', [ProductController::class, 'getTrashed']);
+
+    Route::post('/logout', [UserController::class, 'logout']);
 });
 
-Route::post('/brand', [BrandController::class, 'store']);
 Route::get('/brand', [BrandController::class, 'index']);
 Route::get('/brand/{brand}', [BrandController::class, 'show']);
-Route::put('/brand/{brand}', [BrandController::class, 'update']);
-Route::delete('/brand/{brand}', [BrandController::class, 'destroy']);
-Route::post('/brand/restore/{brand}', [BrandController::class, 'restore']);
-Route::post('/brand/trash', [BrandController::class, 'getTrashed']);
 
-Route::post('/category', [CategoryController::class, 'store']);
 Route::get('/category', [CategoryController::class, 'index']);
 Route::get('/category/{category}', [CategoryController::class, 'show']);
-Route::put('/category/{category}', [CategoryController::class, 'update']);
-Route::delete('/category/{category}', [CategoryController::class, 'destroy']);
-Route::post('/category/restore/{category}', [CategoryController::class, 'restore']);
-Route::post('/category/trash', [CategoryController::class, 'getTrashed']);
 
-Route::post('/product', [ProductController::class, 'store']);
 Route::get('/product', [ProductController::class, 'index']);
 Route::get('/product/{product}', [ProductController::class, 'show']);
-Route::put('/product/{product}', [ProductController::class, 'update']);
-Route::delete('/product/{product}', [ProductController::class, 'destroy']);
-Route::post('/product/restore/{product}', [ProductController::class, 'restore']);
-Route::post('/product/trash', [ProductController::class, 'getTrashed']);
+
+Route::post('/login', [UserController::class, 'login']);
+
